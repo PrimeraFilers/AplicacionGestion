89d74bdf8c4820e529afde1aeabb72374b977a87
AlejandroPascualMoreno <alejandro.s262089@cesurformacion.com>
2022-11-24 18:21:34 +0100

    Logins

--- js/login.js
+++ js/login.js
@@ -5,13 +5,16 @@ console.log(y);
 
 
 function iniciosesion() {
-    if((x.value == "javier@alumnos.com" && y.value == "javier123") || (x.value == "fran@alumnos.com" && y.value == "fran456")) {
-        window.location.href="alumnos.html";
-
-    }else if((x.value == "francisco@profesores.com" && y.value == "francisco") || (x.value == "joseantonio@profesores.com" && y.value == "joseantonio")){
-        window.location.href="profesores.html";
+    if(x.value == "javier@alumnos.com" && y.value == "javier123")  {
+        window.location.href="alumnos-javier.html";
+    }else if(x.value == "fran@alumnos.com" && y.value == "fran456"){
+        window.location.href="alumnos-fran.html";
+    }else if(x.value == "francisco@profesores.com" && y.value == "francisco"){
+        window.location.href="profesores-francisco.html";
+    }else if(x.value == "joseantonio@profesores.com" && y.value == "joseantonio"){
+        window.location.href="profesores-joseantonio.html";
     }else{
-        alert("Usuario y contraseña incorrectos");
+    alert("Usuario y contraseña incorrectos");
     }
 }
 
