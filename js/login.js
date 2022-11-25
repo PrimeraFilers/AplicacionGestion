5ef1e8b3b6ab0b0efb00c5a504bfcecbe0bf1b27
AlejandroPascualMoreno <alejandro.s262089@cesurformacion.com>
2022-11-25 19:53:57 +0100

    Logins 2

--- js/login.js
+++ js/login.js
@@ -1,22 +1,45 @@
 var x = document.querySelector(".correo");
-console.log(x); 
+console.log(x);
 var y = document.querySelector(".psswd");
 console.log(y);
 
 
 function iniciosesion() {
-    if(x.value == "javier@alumnos.com" && y.value == "javier123")  {
-        window.location.href="alumnos-javier.html";
-    }else if(x.value == "fran@alumnos.com" && y.value == "fran456"){
-        window.location.href="alumnos-fran.html";
-    }else if(x.value == "francisco@profesores.com" && y.value == "francisco"){
-        window.location.href="profesores-francisco.html";
-    }else if(x.value == "joseantonio@profesores.com" && y.value == "joseantonio"){
-        window.location.href="profesores-joseantonio.html";
-    }else{
-    alert("Usuario y contraseña incorrectos");
-    }
+    if (x.value == "javier@alumnos.com" && y.value == "javier123") {
+        window.location.href = "alumnos-javier.html";
+    } else if (x.value == "fran@alumnos.com" && y.value == "fran456") {
+        window.location.href = "alumnos-fran.html";
+    } else if (x.value == "francisco@profesores.com" && y.value == "francisco") {
+        window.location.href = "profesores-francisco.html";
+    } else if (x.value == "joseantonio@profesores.com" && y.value == "joseantonio") {
+        window.location.href = "profesores-joseantonio.html";
+    } 
 }
 
 var z = document.querySelector(".enter").addEventListener("click", iniciosesion);
+var a = document.querySelector(".correo");
+var b = document.querySelector(".psswd");
+a.addEventListener("blur",vaciadax);
+b.addEventListener("blur",vaciaday);
+var c = document.querySelector(".enter").addEventListener("click", textada);
 
+
+function textada(){
+    if (x.value!=="javier@alumnos.com" || y.value!=="javier123"){
+        document.querySelector(".emailada").textContent="Correo electrónico o contraseña incorrectos";
+    }
+}
+function vaciadax(){
+    if (x.value==""){
+        document.querySelector(".emailada").textContent="Por favor introduzca el correo electrónico";
+    }else{
+        document.querySelector(".emailada").textContent="";
+    }
+}
+function vaciaday(){
+    if (y.value==""){
+        document.querySelector(".contrasada").textContent="Por favor introduzca la contraseña";
+    }else{
+        document.querySelector(".contrasada").textContent="";
+    }
+}
