9b4ee5c79fb1313585bbd970b2157936edef7fa8
AlejandroPascualMoreno <alejandro.s262089@cesurformacion.com>
2023-02-23 19:59:54 +0100

    La inspirada

--- V2/js/login.js
+++ V2/js/login.js
@@ -5,14 +5,20 @@ console.log(y);
 
 
 function iniciosesion() {
-    if (x.value == "javier@alumnos.com" && y.value == "javier123") {
-        window.location.href = "alumno-javier/index.html";
-    } else if (x.value == "fran@alumnos.com" && y.value == "fran123") {
-        window.location.href = "alumno-fran/index.html";
+    if (x.value == "alvaro@alumnos.com" && y.value == "alvaro123") {
+        window.location.href = "index-alumno.html";
+    } else if (x.value == "ermejildo@alumnos.com" && y.value == "ermejildo123") {
+        window.location.href = "index-alumno.html";
+    }else if (x.value == "goku@alumnos.com" && y.value == "goku123") {
+        window.location.href = "index-alumno.html";
+    }else if (x.value == "javier@alumnos.com" && y.value == "javier123") {
+        window.location.href = "index-alumno.html";
+    }else if (x.value == "lizzy@alumnos.com" && y.value == "lizzy123") {
+        window.location.href = "index-alumno.html";
     } else if (x.value == "francisco@profesores.com" && y.value == "francisco123") {
-        window.location.href = "profesor-francisco/index.html";
+        window.location.href = "index-profesor.html";
     } else if (x.value == "joseantonio@profesores.com" && y.value == "joseantonio123") {
-        window.location.href = "profesor-joseantonio/index.html";
+        window.location.href = "index-profesor.html";
     } 
 }
 
@@ -25,9 +31,15 @@ function vaciadax(){
     if (x.value==""){
         document.querySelector(".emailada").textContent="Por favor introduzca el correo electrónico";
         document.querySelector(".emailada").style.borderColor="red";
-    } else if (x.value!=="javier@alumnos.com") {
+    } else if (x.value!=="alvaro@alumnos.com") {
         document.querySelector(".emailada").textContent="Correo electrónico o contraseña incorrectos";
-    }  else if (x.value !== "fran@alumnos.com") {
+    } else if (x.value !== "ermejildo@alumnos.com") {
+        document.querySelector(".emailada").textContent="Correo electrónico o contraseña incorrectos";
+    } else if (x.value !== "goku@alumnos.com") {
+        document.querySelector(".emailada").textContent="Correo electrónico o contraseña incorrectos";
+    } else if (x.value !== "javier@alumnos.com") {
+        document.querySelector(".emailada").textContent="Correo electrónico o contraseña incorrectos";
+    } else if (x.value !== "lizzy@alumnos.com") {
         document.querySelector(".emailada").textContent="Correo electrónico o contraseña incorrectos";
     } else if (x.value !== "francisco@profesores.com") {
         document.querySelector(".emailada").textContent="Correo electrónico o contraseña incorrectos";
@@ -39,9 +51,15 @@ function vaciadax(){
 function vaciaday(){
     if (y.value==""){
         document.querySelector(".contrasada").textContent="Por favor introduzca la contraseña";
-    }  else if ( y.value!=="javier123") {
+    }  else if ( y.value!=="alvaro123") {
         document.querySelector(".emailada").textContent="Correo electrónico o contraseña incorrectos";
-    }  else if ( y.value !== "fran456") {
+    }  else if ( y.value !== "ermejildo123") {
+        document.querySelector(".emailada").textContent="Correo electrónico o contraseña incorrectos";
+    }else if ( y.value !== "goku123") {
+        document.querySelector(".emailada").textContent="Correo electrónico o contraseña incorrectos";
+    }else if ( y.value !== "javier123") {
+        document.querySelector(".emailada").textContent="Correo electrónico o contraseña incorrectos";
+    }else if ( y.value !== "lizzy123") {
         document.querySelector(".emailada").textContent="Correo electrónico o contraseña incorrectos";
     } else if ( y.value !== "francisco") {
         document.querySelector(".emailada").textContent="Correo electrónico o contraseña incorrectos";
@@ -49,3 +67,34 @@ function vaciaday(){
         document.querySelector(".emailada").textContent="Correo electrónico o contraseña incorrectos";
     }
 }
+
+
+let plantilla = document.querySelector("template"); 
+let contenedor = document.querySelector("tbody");
+let ermejildada = "json/ermejildo.json";
+  /* Importación de datos */
+function cargarDatos(jsonada) {
+  fetch(jsonada)
+  .then(response => response.json())
+    .then(data => {
+      data.forEach(function (el) {
+
+        let wrap = document.createElement("tr");
+        let actividada = plantilla.content.cloneNode(true);
+        
+        actividada.querySelector(".fecha").textContent = el.fecha;
+        actividada.querySelector(".tipo").textContent = el.tipoPractica;
+        actividada.querySelector(".totalHoras").textContent = el.totalHoras;
+        actividada.querySelector(".actividad").textContent = el.actividadRealizada;
+        actividada.querySelector(".observaciones").textContent = el.observacionesIncidencias;
+
+        wrap.appendChild(actividada);       
+        contenedor.appendChild(wrap);
+        
+
+      });
+    })
+    .catch(err => {
+        alert("Hubo un error: " + err + ". Recargue la página.");
+    });
+  };
