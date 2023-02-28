ab44151b444332745ba19f26d7d8ed4761bcc0a8
agonzalezrando <93986162+AlvaroCesur@users.noreply.github.com>
2023-02-28 19:01:29 +0100

    Adicion de boton Eliminar, Editar y Add

--- V2/js/actividades1.js
+++ V2/js/actividades1.js
@@ -5,6 +5,7 @@ function cargarDatos() {
   fetch("json/alvaro.json")
   .then(response => response.json())
     .then(data => {
+      let numeroElemento = 1;
       data.forEach(function (el) {
 
         let wrap = document.createElement("tr");
@@ -16,9 +17,10 @@ function cargarDatos() {
         actividada.querySelector(".actividad").textContent = el.actividadRealizada;
         actividada.querySelector(".observaciones").textContent = el.observacionesIncidencias;
 
-        wrap.appendChild(actividada);       
+        wrap.appendChild(actividada);
+        wrap.classList.add(numeroElemento);
+        numeroElemento++; 
         contenedor.appendChild(wrap);
-        
 
       });
     })
@@ -27,4 +29,51 @@ function cargarDatos() {
     });
   };
 
-  document.addEventListener("DOMContentLoaded",cargarDatos);
+  document.addEventListener("DOMContentLoaded", cargarDatos);
+
+  /* Insercion de datos */
+  const botonInsert = document.querySelector(".insert");
+  const form = document.querySelector('#insertValues');
+
+  botonInsert.addEventListener("click", () => {
+    form.addEventListener('submit', event => {
+  
+      // Se obtienen los datos de las celdas rellenadas
+      const newDate = document.querySelector(".newDate").value;
+      const newTraining = document.querySelector(".newTraining").value;
+      const newTime = document.querySelector(".newTime").value;
+      const newActivity = document.querySelector(".newActivity").value;
+      const newObservation = document.querySelector(".newObservation").value;
+
+      if (newDate && newTraining && newTime && newActivity != "") {
+
+        let wrap = document.createElement("tr");
+  
+        let nuevaActividad = plantilla.content.cloneNode(true);
+    
+        nuevaActividad.querySelector(".fecha").textContent = newDate;
+        nuevaActividad.querySelector(".tipo").textContent = newTraining;
+        nuevaActividad.querySelector(".totalHoras").textContent = newTime;
+        nuevaActividad.querySelector(".actividad").textContent = newActivity;
+        nuevaActividad.querySelector(".observaciones").textContent = newObservation;
+    
+        wrap.appendChild(nuevaActividad);       
+        contenedor.appendChild(wrap);
+
+      }
+  
+    });
+  });
+  
+  /* Edicion de datos */  
+  
+
+  /* Eliminacion de datos */
+  const buttonsDelete = document.querySelectorAll(".delete");
+  buttonsDelete.forEach(function(boton) {
+    boton.addEventListener("click", function() {
+      let fila = this.closest("tr");
+      fila.remove();
+    });
+  });
+  
