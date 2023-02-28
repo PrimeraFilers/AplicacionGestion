c4be1a3d23c061d87b848cd03bd5ca667e1f4642
Lizzy <alicia.s273596@cesurformacion.com>
2023-02-28 21:37:12 +0100

    Editada

--- V2/js/actividades1.js
+++ V2/js/actividades1.js
@@ -61,23 +61,46 @@ function cargarDatos() {
 
       }
     });
+
+    const tablaContainer = document.getElementById("tabla-container");
   
   /* Edicion de datos */  
-  
+  tablaContainer.addEventListener("click", function(event) {
+    if (event.target.classList.contains("edit")) {
+      let fila = event.target.closest("tr");
+      let celdas = fila.querySelectorAll("td");
+      celdas.forEach(function(celda, index) {
+        if (index < celdas.length - 1) { // Evitar que el último botón se edite
+          let contenidoAnterior = celda.innerHTML;
+          celda.innerHTML = `<input type="text" value="${contenidoAnterior}">`;
+        }
+      });
+      fila.classList.add("editing");
+      let botonGuardar = fila.querySelector(".save");
+      botonGuardar.style.display = "inline-block";
+    } else if (event.target.classList.contains("save")) {
+      let fila = event.target.closest("tr");
+      let celdas = fila.querySelectorAll("td");
+      celdas.forEach(function(celda, index) {
+        if (index < celdas.length - 1) { // Evitar que el último botón se edite
+          let input = celda.querySelector("input");
+          celda.innerHTML = input.value;
+        }
+      });
+      fila.classList.remove("editing");
+      let botonGuardar = fila.querySelector(".save");
+      botonGuardar.style.display = "none";
+    }
+  });
 
   /* Eliminacion de datos */
-  const tablaContainer = document.getElementById("tabla-container");
+
   tablaContainer.addEventListener("click", function(event) {
     if (event.target.classList.contains("delete")) {
       let fila = event.target.closest("tr");
       fila.remove();
-<<<<<<< HEAD
     }
   });
 
   
   
-=======
-    });
-  });
->>>>>>> eebdb88cf6c281d1bf8546f08479c37efabdb981
